543fff6155e5f614e1a03101be160845f83301ba
NIKDEK <>
2021-06-22 11:39:03 -0400

    New methods added

--- server.py
+++ server.py
@@ -6,11 +6,10 @@ app = Flask(__name__)
 
 app.secret_key = b"_93JE93JR9R49RTI"
 
-info = {
-    'Users':['TEST1']
-}
+info = {'Users':['TEST1']}
+credentials = {'creds': ['admin:S83H4448JD8']}
 
-@app.route('/users', methods=['GET', 'POST'])
+@app.route('/users', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def users():
     print(request.headers)
     if request.method == 'GET':
@@ -18,3 +17,9 @@ def users():
     elif request.method == 'POST':
         print(request.get_data())
         return 'POSTED'
+    elif request.method == 'PUT':
+        return 'PUTTED'
+    elif request.method == 'DELETED':
+        return 'DELETED'
+    else:
+        return 'Method not supported'
