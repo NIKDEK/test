4a1d76aa0dc8afc152a3619745c6bdb0cd374633
NIKDEK <>
2021-06-25 16:05:36 -0400

    TEST

--- client.py
+++ client.py
@@ -9,14 +9,21 @@ with open('apipass.json', 'r') as api_info:
     api_info.close()
 auth_b64 = base64.b64decode(creds)
 
+def err_handler(res):
+    return 'API call error: {}'.format(res)
+
 def get():
     header  = {'Authorization': '{} {}'.format(auth_type, auth_b64)}
     res = requests.get('http://127.0.0.1:5000/users', headers=header)
+    if res.status_code != 200:
+        raise Exception(err_handler(res))
     return res.text
 
 def post(jsn):
     content = {'Content-Type': 'application/json'}
     res = requests.post('http://127.0.0.1:5000/users', headers=content, data=json.dumps(jsn))
+    if res.status_code != 200:
+        raise Exception(err_handler(res))
     return str(res.status_code)
 
 inf = json.loads(get())
